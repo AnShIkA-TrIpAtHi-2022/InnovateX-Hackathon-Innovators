f7c913516cb7541a008e8f8f4f9f54c6ef9a4e4c
Anshika Gupta <anshikagupta0762@gmail.com>
2024-11-21 11:31:38 +0530

    Updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('home')
